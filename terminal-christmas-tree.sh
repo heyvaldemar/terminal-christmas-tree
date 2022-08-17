db4a5b42f319e1ded3c4e5dfdf1a613f2ae50fc1
Vladimir Mikhalev <10498744+heyValdemar@users.noreply.github.com>
2022-08-17 14:16:28 -0400

    Christmas Tree for Terminal

--- terminal-christmas-tree.sh
+++ terminal-christmas-tree.sh
@@ -2,9 +2,38 @@
 
 # Christmas Tree for Terminal
 
-# Vladimir Mikhalev
-# callvaldemar@gmail.com
-# www.heyvaldemar.com
+# Author
+# hey, I’m Vladimir Mikhalev, but my friends call me Valdemar.
+
+# My website with detailed IT guides: https://www.heyvaldemar.com/
+# Follow me on YouTube: https://www.youtube.com/channel/UCf85kQ0u1sYTTTyKVpxrlyQ?sub_confirmation=1
+# Follow me on Twitter: https://twitter.com/heyValdemar
+# Follow me on Instagram: https://www.instagram.com/heyvaldemar/
+# Follow me on Facebook: https://www.facebook.com/heyValdemarFB/
+# Follow me on TikTok: https://www.tiktok.com/@heyvaldemar
+# Follow me on [LinkedIn](https://www.linkedin.com/in/heyvaldemar/)
+# Follow me on [GitHub](https://github.com/heyvaldemar)
+
+# Communication
+# Chat with IT pros on Discord: https://discord.com/invite/D7fGMYjdR9
+# Chat with IT pros on Telegram: https://t.me/heyValdemarCOMchat
+# Reach me at ask@sre.gg
+
+# Give Thanks
+# Support on GitHub: https://github.com/sponsors/heyValdemar
+# Support on Patreon: https://www.patreon.com/heyValdemar
+# Support on BuyMeaCoffee: https://www.buymeacoffee.com/heyValdemar
+# Support on Ko-fi: https://ko-fi.com/heyValdemar
+# Support on PayPal: https://www.paypal.com/paypalme/heyValdemarCOM
+
+# Download terminal-christmas-tree.sh using the command:
+# curl -O https://github.com/heyValdemar/terminal-christmas-tree/blob/main/terminal-christmas-tree.sh
+
+# Enable execution of the file terminal-christmas-tree.sh using the command:
+# chmod +x terminal-christmas-tree.sh
+
+# Start the terminal-christmas-tree.sh using the command:
+# ./chmod +x terminal-christmas-tree.sh
 
 trap "tput reset; tput cnorm; exit" 2
 clear
