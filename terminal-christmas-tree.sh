4746547eb6c12c6d99b839a0e68f11d3d8f13862
Vladimir Mikhalev <10498744+heyvaldemar@users.noreply.github.com>
2025-01-17 11:43:29 -0500

    Christmas Tree for Terminal

--- terminal-christmas-tree.sh
+++ terminal-christmas-tree.sh
@@ -1,43 +1,5 @@
 #!/bin/bash
 
-# Christmas Tree for Terminal
-
-# Author
-# I’m Vladimir Mikhalev, the Docker Captain, but my friends can call me Valdemar.
-# https://www.docker.com/captains/vladimir-mikhalev/
-
-# My website with detailed IT guides: https://www.heyvaldemar.com/
-# Follow me on YouTube: https://www.youtube.com/channel/UCf85kQ0u1sYTTTyKVpxrlyQ?sub_confirmation=1
-# Follow me on Twitter: https://twitter.com/heyValdemar
-# Follow me on Instagram: https://www.instagram.com/heyvaldemar/
-# Follow me on Threads: https://www.threads.net/@heyvaldemar
-# Follow me on Mastodon: https://mastodon.social/@heyvaldemar
-# Follow me on Bluesky: https://bsky.app/profile/heyvaldemar.bsky.social
-# Follow me on Facebook: https://www.facebook.com/heyValdemarFB/
-# Follow me on TikTok: https://www.tiktok.com/@heyvaldemar
-# Follow me on LinkedIn: https://www.linkedin.com/in/heyvaldemar/
-# Follow me on GitHub: https://github.com/heyvaldemar
-
-# Communication
-# Chat with IT pros on Discord: https://discord.gg/AJQGCCBcqf
-# Reach me at ask@sre.gg
-
-# Give Thanks
-# Support on GitHub: https://github.com/sponsors/heyValdemar
-# Support on Patreon: https://www.patreon.com/heyValdemar
-# Support on BuyMeaCoffee: https://www.buymeacoffee.com/heyValdemar
-# Support on Ko-fi: https://ko-fi.com/heyValdemar
-# Support on PayPal: https://www.paypal.com/paypalme/heyValdemarCOM
-
-# Download terminal-christmas-tree.sh using the command:
-# curl -O https://github.com/heyValdemar/terminal-christmas-tree/blob/main/terminal-christmas-tree.sh
-
-# Enable execution of the file terminal-christmas-tree.sh using the command:
-# chmod +x terminal-christmas-tree.sh
-
-# Start the terminal-christmas-tree.sh using the command:
-# ./terminal-christmas-tree.sh
-
 trap "tput reset; tput cnorm; exit" 2
 clear
 tput civis
